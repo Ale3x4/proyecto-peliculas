784ba3ce97957ca709cf1f0a5d150c638db8c76c
Ale3x4 <alurrego03@gmail.com>
2024-06-01 11:40:42 -0500

    first commit

---
+++ src/app/contactos/contactos.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contactos',
+  templateUrl: './contactos.component.html',
+  styleUrls: ['./contactos.component.css']
+})
+export class ContactosComponent {
+  contactForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      subject: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+}
